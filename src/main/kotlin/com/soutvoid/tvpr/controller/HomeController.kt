0e3dc094af86103fb0a5760dec28e9e9a9f3d7af
nadraliev <nadraliev@gmail.com>
2017-09-07 22:43:32 +0300

    test

--- src/main/kotlin/com/soutvoid/tvpr/controller/HomeController.kt
+++ src/main/kotlin/com/soutvoid/tvpr/controller/HomeController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView
 class HomeController {
 
     @RequestMapping("/")
-    fun index(model: Model) : ModelAndView {
+    fun index() : ModelAndView {
         return ModelAndView("index")
     }
 
