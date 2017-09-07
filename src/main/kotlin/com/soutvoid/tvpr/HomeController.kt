898d24eb14bf10303d0a4de9b0c47d26b13f4cd6
nadraliev <nadraliev@gmail.com>
2017-09-07 21:36:50 +0300

    added home page

--- src/main/kotlin/com/soutvoid/tvpr/HomeController.kt
+++ src/main/kotlin/com/soutvoid/tvpr/HomeController.kt
@@ -1,16 +1,18 @@
 package com.soutvoid.tvpr
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class HomeController {
 
     @RequestMapping("/")
-    @ResponseBody
-    fun home() : String {
-        return "Hellp"
+    fun index(model: Model, @RequestParam(name = "name", required = false, defaultValue = "Name") name: String) : ModelAndView {
+        return ModelAndView("index", "name", name)
     }
 
 }
--- src/main/kotlin/com/soutvoid/tvpr/TvprApplication.kt
+++ src/main/kotlin/com/soutvoid/tvpr/TvprApplication.kt
@@ -2,9 +2,10 @@ package com.soutvoid.tvpr
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import kotlin.coroutines.experimental.buildIterator
 
 @SpringBootApplication
 class TvprApplication
