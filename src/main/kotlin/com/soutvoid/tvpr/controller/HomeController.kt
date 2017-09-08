81f109644febd172d10d8595e2c31d17a6daac3d
nadraliev <nadraliev@gmail.com>
2017-09-08 10:06:33 +0300

    test

--- src/main/kotlin/com/soutvoid/tvpr/controller/HomeController.kt
+++ src/main/kotlin/com/soutvoid/tvpr/controller/HomeController.kt
@@ -3,16 +3,18 @@ package com.soutvoid.tvpr.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.servlet.ModelAndView
 
+@RequestMapping("/")
 @Controller
 class HomeController {
 
-    @RequestMapping("/")
-    fun index() : ModelAndView {
-        return ModelAndView("index")
+    @RequestMapping(method = arrayOf(RequestMethod.GET))
+    fun index() : String {
+        return "index"
     }
 
     @RequestMapping("/test")
